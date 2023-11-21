90ef95ca3f191135bc0bc9d17d9c46ade7035cee
indacurdev <70963212+indacurdev@users.noreply.github.com>
2023-11-21 13:33:30 -0400

    seccion de marcas home

--- components/partials/homepage/autopart/AutopartBestSaleBrand.jsx
+++ components/partials/homepage/autopart/AutopartBestSaleBrand.jsx
@@ -5,77 +5,90 @@ const TechnologyBestSaleBrands = () => (
     <div className="ps-best-sale-brands ps-section--furniture">
         <div className="container">
             <div className="ps-section__header">
-                <h3>Nuestras tiendas</h3>
+                <h3>Trabajamos con las mejores marcas</h3>
             </div>
             <div className="ps-section__content">
                 <ul className="ps-image-list">
                     <li>
                         <Link href="/shop">
                             <a>
-                                <img src="/static/img/brand/2-1.jpg" alt="martfury" />
+                                <img src="/static/img/marcas/acd_log_01.jpg" alt="martfury" />
                             </a>
                         </Link>
                     </li>
                     <li>
                         <Link href="/shop">
                             <a>
-                                <img src="/static/img/brand/2-2.jpg" alt="martfury" />
+                                <img 
+                                    src="/static/img/marcas/logo-friparts-2x.png" 
+                                    alt="martfury" 
+                                    style={{maxWidth: '90%'}}
+                                />
                             </a>
                         </Link>
                     </li>
                     <li>
                         <Link href="/shop">
                             <a>
-                                <img src="/static/img/brand/2-3.jpg" alt="martfury" />
+                                <img src="/static/img/marcas/DUNCAN.png" alt="martfury" />
                             </a>
                         </Link>
                     </li>
                     <li>
                         <Link href="/shop">
                             <a>
-                                <img src="/static/img/brand/2-4.jpg" alt="martfury" />
+                                <img src="/static/img/marcas/encava-header.png" alt="martfury" />
                             </a>
                         </Link>
                     </li>
                     <li>
                         <Link href="/shop">
                             <a>
-                                <img src="/static/img/brand/2-5.jpg" alt="martfury" />
+                                <img src="/static/img/marcas/logo.png" alt="martfury" />
+                            </a>
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link href="/shop">
+                            <a>
+                                <img src="/static/img/marcas/monroe.png" alt="martfury" />
                             </a>
                         </Link>
                     </li>
                     <li>
                         <Link href="/shop">
                             <a>
-                                <img src="/static/img/brand/2-6.jpg" alt="martfury" />
+                                <img 
+                                    src="/static/img/marcas/mopar-1-2500x2459.jpg" 
+                                    alt="martfury" 
+                                    style={{maxWidth: '60%'}}
+                                />
                             </a>
                         </Link>
                     </li>
                     <li>
                         <Link href="/shop">
                             <a>
-                                <img src="/static/img/brand/2-7.jpg" alt="martfury" />
+                                <img src="/static/img/marcas/png-transparent-motorcraft-hd-logo.png" alt="martfury" />
                             </a>
                         </Link>
                     </li>
                     <li>
                         <Link href="/shop">
                             <a>
-                                <img src="/static/img/brand/2-8.jpg" alt="martfury" />
+                                <img 
+                                    src="/static/img/marcas/PngItem_4419764.png" 
+                                    alt="martfury" 
+                                    style={{maxWidth: '90%'}}
+                                />
                             </a>
                         </Link>
                     </li>
                     <li>
                         <Link href="/shop">
                             <a>
-                                <img src="/static/img/brand/2-9.jpg" alt="martfury" />
-                            </a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/shop">
-                            <a>
-                                <img src="/static/img/brand/2-10.jpg" alt="martfury" />
+                                <img src="/static/img/marcas/Untitled1-700x232.png" alt="martfury" />
                             </a>
                         </Link>
                     </li>
