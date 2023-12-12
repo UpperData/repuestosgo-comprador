d409425b8a15f91ba239752eaa97befd99db8193
indacurdev <70963212+indacurdev@users.noreply.github.com>
2023-12-12 14:50:57 -0400

    profile layout

--- pages/account/profile.jsx
+++ pages/account/profile.jsx
@@ -12,6 +12,8 @@ const MyAccountPage = () => {
     const auth = useAuth();
     const user = auth.session.user;
 
+    console.log('USER', user);
+
     const breadCrumb = [
         {
             text: 'Inicio',
@@ -54,21 +56,112 @@ const MyAccountPage = () => {
                                 </div>
                                 <div className="col-lg-9 col-md-12">
                                     <div className="card">
-                                        <div className="card-body">
-                                            <div className="ps-widget__header">
-                                                {/* 
-                                                    <img src="/static/img/users/3.jpg" />
-                                                */}
-                                                
-                                                <figure>
-                                                    <figcaption>
-                                                        {user.people.firstName} {user.people.lastName}
-                                                    </figcaption>
-                                                    <h3>
-                                                        {user.email}
-                                                    </h3>
-                                                </figure>
-                                                
+                                        <div className="card-body py-5">
+                                            <div className="row">
+                                                <div className="col-lg-3">
+                                                    <div className="content-profile-photo">
+                                                        
+                                                    </div>
+                                                </div>
+                                                <div className="col-lg-9">
+
+                                                    <div className="ps-widget__header">
+                                                        {/* 
+                                                            <img src="/static/img/users/3.jpg" />
+                                                        */}
+                                                        
+                                                        <figure>
+                                                            <figcaption>
+                                                                {`${user.people.firstName} ${user.people.lastName}`}
+                                                                {user.isConfirmed &&
+                                                                    <i className="fa fa-check-circle ml-2 text-success"></i>
+                                                                }
+                                                            </figcaption>
+                                                            <h3>
+                                                                {user.email}
+                                                            </h3>
+                                                            <figcaption>
+                                                                {user.people.document.nationality === 'v' ? 'Venezolano' : 'Extranjero'}
+                                                            </figcaption>
+                                                        </figure>
+                                                        
+                                                    </div>
+                                                    <hr />
+                                                    <h4 className='font-weight-normal'>
+                                                        Datos personales
+                                                    </h4>
+
+                                                    <div className="row mb-3">
+                                                        <div className="col-lg-6 mb-3">
+                                                            <label htmlFor="">
+                                                                Nombre
+                                                            </label>
+                                                            <input 
+                                                                readOnly
+                                                                type="text" 
+                                                                className='form-control'
+                                                                value={user.people.firstName}
+                                                            />
+                                                        </div>
+                                                        <div className="col-lg-6 mb-3">
+                                                            <label htmlFor="">
+                                                                Apellido
+                                                            </label>
+                                                            <input 
+                                                                readOnly
+                                                                type="text" 
+                                                                className='form-control'
+                                                                value={user.people.lastName}
+                                                            />
+                                                        </div>
+                                                    </div>
+
+                                                    <h4 className='font-weight-normal'>
+                                                        Identificación
+                                                    </h4>
+                                                    <div className="row">
+                                                        <div className="col-lg-4 mb-3">
+                                                            <label htmlFor="">
+                                                                Género
+                                                            </label>
+                                                            <input 
+                                                                readOnly
+                                                                type="text" 
+                                                                className='form-control'
+                                                                value={user.people.document.gender === 'h' ? 'Hombre' : 'Mujer'}
+                                                            />
+                                                        </div>
+                                                        <div className="col-lg-4 mb-3">
+                                                            <label htmlFor="">
+                                                                Documento de identidad
+                                                            </label>
+                                                            <input 
+                                                                readOnly
+                                                                type="text" 
+                                                                className='form-control'
+                                                                value={user.people.document.number}
+                                                            />
+                                                        </div>
+                                                        <div className="col-lg-4 mb-3">
+                                                            <label htmlFor="">
+                                                                Estado civil
+                                                            </label>
+                                                            <input 
+                                                                readOnly
+                                                                type="text" 
+                                                                className='form-control'
+                                                                value={user.people.document.civil.name}
+                                                            />
+                                                        </div>
+                                                    </div>
+
+                                                    <div className="text-right pt-4">
+                                                        <button className="ps-btn text-white">
+                                                            Editar perfil
+                                                        </button>
+                                                    </div>
+
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
